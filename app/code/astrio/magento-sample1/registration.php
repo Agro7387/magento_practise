ca1d0d4a78e4f691afde716e6182363704325abd
Tuktarov Denis <tuktarov-dg@yandex.ru>
2022-04-01 09:57:01 +0400

    Practice Task 1

---
+++ app/code/astrio/magento-sample1/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Astrio_Sample1',
+    __DIR__
+);
---
+++ app/code/astrio/magento-sample2/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Astrio_Sample2',
+    __DIR__
+);
